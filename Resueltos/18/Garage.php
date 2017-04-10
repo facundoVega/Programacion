e64b66ed7c9081c44b593523f153f0613da3084c
mibanez <mibanez@adistec.com>
2017-04-09 22:17:42 -0300

    modif

--- Resueltos/18/Garage.php
+++ Resueltos/18/Garage.php
@@ -18,21 +18,30 @@ include_once "Auto.php";
 
           echo "Razon social: ".$this->_razonSocial."<br>";
           echo "Precio por Hora: ".$this->_precioPorHora."<br>";
+          echo "-----<br>";
+         /* for($i=0;$i<count($this->_autos);$i++){
+             
+              echo "Marca: ".$this->_autos[$i]->getMarca()."<br>";
+              echo "Color: ".$this->_autos[$i]->getColor()."<br>";
+              echo "Precio: ".$this->_autos[$i]->getPrecio()."<br>";
+              echo "Fecha: ".$this->_autos[$i]->getFecha()."<br>";
+              echo "------------------------<br>";         
 
-          for($i=0;$i<count($this->_autos);$i++){
-
-              echo "Marca: ".$this->_autos[$i].getMarca()."<br>";
-              echo "Color: ".$this->_autos[$i].getColor()."<br>";
-              echo "Precio: ".$this->_autos[$i].getPrecio()."<br>";
-              echo "Fecha: ".$this->_autos[$i].getFecha()."<br>";
-
+          } unset deja mal los indices despues de borrar y tira error al entrar en un get*/
+          foreach ($this->_autos as $item) {
+               echo "Marca: ".$item->getMarca()."<br>";
+               echo "Color: ".$item->getColor()."<br>";
+               echo "Precio: ".$item->getPrecio()."<br>";
+               echo "Fecha: ".$item->getFecha()."<br>";
+               echo "------------------------<br>";      
           }
+         
       }
 
       public function Equals($objAuto){
            
            for($i=0;$i<count($this->_autos);$i++){
-               if($this->_autos[$i].Equals($objAuto)){
+               if($this->_autos[$i]->Equals($objAuto)){
                    return true;
                }
            }
@@ -42,8 +51,9 @@ include_once "Auto.php";
       public function Add ($objAuto){
          if($this->Equals($objAuto) == false){
            array_push($this->_autos,$objAuto);
+           echo "Se agrego auto al garage <br>";
          }else{
-             echo "El auto ya se encuentra en el garage";
+             echo "El auto ya se encuentra en el garage <br>";
          }
       }
 
@@ -52,13 +62,13 @@ include_once "Auto.php";
          if($this->Equals($objAuto)){
 
             for($i=0;$i<count($this->_autos);$i++){
-                 if($this->_autos[$i].Equals($objAuto)){
+                 if($this->_autos[$i]->Equals($objAuto)){
                      unset($this->_autos[$i]);
-                     echo "se elimino auto del garage";
+                     echo "se elimino auto del garage <br>";
                 }
              }
          }else{
-           echo "el auto no se encuentra en el garage";
+           echo "el auto no se encuentra en el garage <br>";
          }
         
       }
--- Resueltos/18/testGarage.php
+++ Resueltos/18/testGarage.php
@@ -1,3 +1,31 @@
 <?php
+
 include_once "Garage.php";
+
+$auto_1 = new Auto("BMW","Rojo",100000);
+$auto_2 = new Auto("BMW","Blanco",200000);
+$auto_3 = new Auto("Ford","Gris",300000);
+$auto_4 = new Auto("Ford","Azul",400000);
+$auto_5 = new Auto("Fiat","Verde",500000);
+
+$garage_1 = new Garage ("Garage test srl",10.1);
+
+$garage_1->Add($auto_1);
+$garage_1->Add($auto_2);
+$garage_1->Add($auto_3);
+$garage_1->Add($auto_4);
+$garage_1->Add($auto_5);
+echo "________________";
+echo "<br>";
+$garage_1->MostrarGarage();
+echo "<br>";
+echo "________________";
+echo "<br>";
+$garage_1->Remove($auto_3);
+echo "<br>";
+echo "________________";
+echo "<br>";
+//var_dump($garage_1); unset elimina el indice tambien y deja mal el array y al momento de entrar en un obj del array tira error utilizar foreach
+$garage_1->MostrarGarage();
+
 ?>
