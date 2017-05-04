2f8ca6bb10e0776bfbad2453df59d9db0066510e
mattibz <matt.ibz15@gmail.com>
2017-05-04 15:04:22 -0300

    fix

--- clase_03/Resueltos/22/destino.php
+++ clase_03/Resueltos/22/destino.php
@@ -1,6 +1,6 @@
 <?php
 $origen = $_GET['origen'];
-$destino =date('Y_m_d_h_i_s').".txt";
+$destino =date('Y_m_d_h_i_s').".txt"; // si no existe el archivo destino lo crea
 if(copy($origen,$destino)){
     echo "<b>Se copio correctamente el archivo</b>";
 }
