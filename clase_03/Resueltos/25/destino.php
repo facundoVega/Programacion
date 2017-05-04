57bd099201ff2671ec9dc8cc8eab4ca8dec13af6
mattibz <matt.ibz15@gmail.com>
2017-05-04 19:28:03 -0300

    add

---
+++ clase_03/Resueltos/25/destino.php
@@ -0,0 +1,10 @@
+<?php
+$color = $_GET['color'];
+echo "<html><head><title>asd</title>".
+"<body style='background-color:".$color."'>".
+"</head>". 
+"<body><a href='http://localhost/Programacion/clase_03/Resueltos/25/'>Volver</a>". 
+"</body>".
+"</html>";
+ 
+?>
