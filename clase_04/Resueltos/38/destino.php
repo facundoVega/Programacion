afc0d1ec5028efc99aba2e59b31af8bfffea4f30
mattibz <matt.ibz15@gmail.com>
2017-05-05 20:33:51 -0300

    fix

--- clase_04/Resueltos/38/destino.php
+++ clase_04/Resueltos/38/destino.php
@@ -1,9 +1,19 @@
 <?php
+define('KB', 1024);
+define('MB', 1048576);
+define('GB', 1073741824);
+define('TB', 1099511627776);
 
 for($i=0;$i<count($_FILES['foto']['name']);$i++){
 
+    if(($_FILES['foto']['type'][$i]!="image/jpeg" && $_FILES['foto']['type'][$i]!= ".jpg") || ($_FILES['foto']['size'][$i]>900*KB)){
+        echo "<b>Archivo no valido: </b>".$_FILES['foto']['name'][$i];
+        continue;
+    }
+    
     $destino = "imagenes/".$_FILES['foto']['name'][$i];
     move_uploaded_file($_FILES['foto']['tmp_name'][$i],$destino);
 }
+
 
 ?>
