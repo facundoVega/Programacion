367c6b4c8831d7209aa09c2806ad51a98febdaca
mattibz <matt.ibz15@gmail.com>
2017-05-05 14:44:07 -0300

    add

---
+++ clase_03/Resueltos/33/destino.php
@@ -0,0 +1,9 @@
+<?php
+echo "<b>Info:</bv><br>";
+echo "Nombre: ".$_GET['nombre']."<br>";
+echo "Apellido: ".$_GET['apellido']."<br>";
+echo "Edad: ".$_GET['edad']."<br>";
+echo "Direccion: ".$_GET['direccion']."<br>";
+echo "Mail: ".$_GET['mail']."<br>";
+echo "Texto: ".$_GET['texto']."<br>";
+?>
